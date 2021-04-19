c6af1cfbe6218cb73fb871cba7da0ae0b381f68b
zuotang <zuo.tang@outlook.com>
2021-04-20 00:04:33 +0800

    update cookie table

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost", //"http://www.storm-money.com",
+      target: "http://localhost", // "http://www.fortunemeow.com", //"http://www.storm-money.com",
       changeOrigin: true,
       pathRewrite: {
         "^/api": "/", // rewrite path
